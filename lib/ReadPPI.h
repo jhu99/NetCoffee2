d053f86b66083f514de5331e34ba62cfdcbeec9f
Jialu HU <jhu@nwpu.edu.cn>
2017-11-13 14:14:03 +0800

    store a pointer in the map

--- lib/ReadPPI.h
+++ lib/ReadPPI.h
@@ -3,14 +3,14 @@
 
 #include <string>
 #include <unordered_map>
-
+#include <vector>
 
 class ReadPPI
 {
 public:
 	ReadPPI(std::string netname, int num_nets);
 	~ReadPPI();
-	std::unordered_map<std::string, std::vector<double> > top_vec;
+	std::unordered_map<std::string, std::vector<double>* > top_vec;
 	
 private:
 	int m_iNumNets;
