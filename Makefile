47b9ddc41a3362209ae7daf6af80da4476c25eff
Jialu HU <jhu@nwpu.edu.cn>
2017-11-27 10:32:27 +0800

    makefile and option

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ endif
 ifeq ($(MODE),Debug)
 CXXFLAGS = -Wall -g3 -DDEBUG -std=c++0x -DVERBOSE -Ilib/
 else
-CXXFLAGS = -Wall -O3 -ffast-math -Ilib/ -std=c++0x -DNDEBUG
+CXXFLAGS = -Wall -O3 -ffast-math -Ilib/ -I/usr/local/include/igraph/ -std=c++0x -DNDEBUG -l/usr/local/lib/libigraph.a
 endif
 all: miteFinderTest patternLearningTest ArgParserTest
 miteFinderTest: tests/miteFinderTest.cpp lib/genome.o lib/mite.o lib/argparser.o
@@ -23,6 +23,8 @@ miteFinderTest: tests/miteFinderTest.cpp lib/genome.o lib/mite.o lib/argparser.o
 patternLearningTest: tests/patternTest.cpp lib/genome.o
 	${CXX} ${CXXFLAGS} -o $@ $^
 ArgParserTest: tests/testargparser.cpp lib/argparser.o
+	${CXX} ${CXXFLAGS} -o $@ $^
+NetCoffee2: test/test.cpp lib/simulate.o lib/Alignment.o lib/argparser.o lib/ReadBitscore.o lib/ReadPPI.o lib/ProteinRel.o
 	${CXX} ${CXXFLAGS} -o $@ $^
 clean:
 	rm lib/*.o
--- test/test.cpp
+++ test/test.cpp
@@ -42,21 +42,21 @@ int main(int argc, const char * argv[])
 	*/
 
 
-    //ArgParser mf_parser;
-    //Option mf_option;
-    //
-    //// Parser the argument.
-    //mf_parser.setName("NetCoffee2", "An application for multiple global network alignment.");
-    //mf_parser.setVerion("1.0.006");
-    //mf_parser.refOption("help", "Show help information.", mf_option.help);
-    //mf_parser.refOption("version", "Show the current version.", mf_option.version);
-    //mf_parser.refOption("input", "The path of an input file.", mf_option.inputfilename, "", true);
-    //mf_parser.refOption("numnet", "The number of ppi networks", mf_option.numNet,3,true);
-    //
-    //if(!mf_parser.run(argc, argv))
-    //    return 1;
+    ArgParser mf_parser;
+    Option mf_option;
     
-	ReadPPI net("int_nets.tab", 3);
+    // Parser the argument.
+    mf_parser.setName("NetCoffee2", "An application for multiple global network alignment.");
+    mf_parser.setVerion("1.0.006");
+    mf_parser.refOption("help", "Show help information.", mf_option.help);
+    mf_parser.refOption("version", "Show the current version.", mf_option.version);
+    mf_parser.refOption("input", "The path of an input file.", mf_option.inputfilename, "", true);
+    mf_parser.refOption("numnet", "The number of ppi networks", mf_option.numNet,3,true);
+    
+    if(!mf_parser.run(argc, argv))
+        return 1;
+    
+	ReadPPI net(mf_option.inputfilename, mf_option.numNet);
 	net.calculate_topologyVector();
 
 	unordered_map<std::string, double* > top = net.top_vec;
